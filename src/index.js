05bcacc159fa9807a7532b36a2702e01aab5a311
casdidier <cheung.didier@gmail.com>
2020-11-02 16:24:59 +0100

    global Styled component

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './app';
-import { GlobalStyles } from './global-styles';
+import { GlobalStyles } from './global-styles'; // styled component Global style
 
 ReactDOM.render(
   <>
