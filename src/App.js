7101f5945026c7f34f5cd2899356649e4e7917f0
casdidier <cheung.didier@gmail.com>
2020-11-02 11:38:22 +0100

    creating files/folders

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
+import React from 'react'
+import Jumbotron from './components/jumbotron'
+
 export default function App() {
   return (
     <div className="App">
       Hello
+      <Jumbotron></Jumbotron>
     </div>
   );
 }
