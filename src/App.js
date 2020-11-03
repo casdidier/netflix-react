48f062d4b1edce7c2dd0d45b7379fd2d7f8877f2
casdidier <cheung.didier@gmail.com>
2020-11-03 16:10:59 +0100

    routes

--- src/App.js
+++ src/App.js
@@ -1,14 +1,22 @@
 import React from 'react'
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+
 import { FaqsContainer } from './containers/faq';
 import { FooterContainer } from './containers/footer';
 import { JumbotronContainer } from './containers/jumbotron';
+import * as ROUTES from './constants/routes';
 
 export default function App() {
   return (
-    <>
-      <JumbotronContainer />
-      <FaqsContainer />
-      <FooterContainer />
-    </>
+    <Router>
+      <Route exact path={ROUTES.HOME}>
+        <JumbotronContainer />
+        <FaqsContainer />
+        <FooterContainer />
+      </Route>
+      <Route exact path='/user'>
+        <p>I am user</p>
+      </Route>
+    </Router>
   );
 }
