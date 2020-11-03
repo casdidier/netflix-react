ad20f5232cb2f8c4b1b86fca37c4971868fa41a5
casdidier <cheung.didier@gmail.com>
2020-11-03 12:09:21 +0100

    clean up

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react'
+import { FooterContainer } from './containers/footer';
 import { JumbotronContainer } from './containers/jumbotron';
 
 export default function App() {
   return (
-    <div className="App">
+    <>
       <JumbotronContainer />
-    </div>
+      <FooterContainer />
+    </>
   );
 }
