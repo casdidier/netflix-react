3a2a063cb856f9d93dc205eba0f9e3745a523663
casdidier <cheung.didier@gmail.com>
2020-11-03 18:00:26 +0100

    header component

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,5 +1,18 @@
 import React from 'react';
+import { Feature, OptForm } from '../components';
+import { HeaderContainer } from '../containers/header';
+import { JumbotronContainer } from '../containers/jumbotron';
+import { FaqsContainer } from '../containers/faqs';
+import { FooterContainer } from '../containers/footer';
 
-export default function Signin(params) {
-  return (<p>Hello Signin</p>)
-};
+export default function Home() {
+  return (
+    <>
+      <HeaderContainer>
+        <JumbotronContainer />
+        <FaqsContainer />
+        <FooterContainer />
+      </HeaderContainer>
+    </>
+  );
+}
