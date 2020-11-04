ee4d3158387cc3353d143546d053d7a892175cb1
casdidier <cheung.didier@gmail.com>
2020-11-04 17:51:55 +0100

    firebase signup

--- src/pages/signup.js
+++ src/pages/signup.js
@@ -18,7 +18,25 @@ export default function SignUp() {
   const handleSignup = (event) => {
     event.preventDefault();
 
-    // Firebase stuff
+    return firebase
+    .auth()
+    .createUserWithEmailAndPassword(emailAddress, password)
+    .then((result) =>
+      result.user
+        .updateProfile({
+          displayName: firstName,
+          photoURL: Math.floor(Math.random() * 5) + 1,
+        })
+        .then(() => {
+          history.push(ROUTES.BROWSE);
+        })
+    )
+    .catch((error) => {
+      setFirstName('');
+      setEmailAddress('');
+      setPassword('');
+      setError(error.message);
+    });
   };
 
   const isInvalid = firstName === '' || password === '' || emailAddress === '';
