13dabe0e7315d8ee92a9f077403bb14fcdfadf5a
casdidier <cheung.didier@gmail.com>
2020-11-04 17:51:36 +0100

    Page signup

--- src/pages/signup.js
+++ src/pages/signup.js
@@ -1,5 +1,67 @@
-import React from 'react';
+import React, { useState, useContext } from 'react';
+import { Form } from '../components';
+import {useHistory} from 'react-router-dom';
+import { HeaderContainer } from '../containers/header';
+import { FooterContainer } from '../containers/footer';
+import * as ROUTES from '../constants/routes';
+import { FirebaseContext } from '../context/firebase';
 
-export default function Signup(params) {
-  return (<p>Hello Sign up</p>)
-};
+export default function SignUp() {
+
+  const history = useHistory();
+  const {firebase} = useContext(FirebaseContext);
+  const [firstName, setFirstName] = useState('');
+  const [emailAddress, setEmailAddress] = useState('');
+  const [password, setPassword] = useState('');
+  const [error, setError] = useState('');
+
+  const handleSignup = (event) => {
+    event.preventDefault();
+
+    // Firebase stuff
+  };
+
+  const isInvalid = firstName === '' || password === '' || emailAddress === '';
+
+  return (
+    <>
+      <HeaderContainer>
+        <Form>
+          <Form.Title>Sign up</Form.Title>
+          {error && <Form.Error data-testid="error">{error}</Form.Error>}
+
+          <Form.Base onSubmit={handleSignup} method="POST">
+            <Form.Input
+              placeholder="First name"
+              value={firstName}
+              onChange={({ target }) => setFirstName(target.value)}
+            />
+            <Form.Input
+              placeholder="Email address"
+              value={emailAddress}
+              onChange={({ target }) => setEmailAddress(target.value)}
+            />
+            <Form.Input
+              type="password"
+              value={password}
+              autoComplete="off"
+              placeholder="Password"
+              onChange={({ target }) => setPassword(target.value)}
+            />
+            <Form.Submit disabled={isInvalid} type="submit" data-testid="sign-up">
+              Sign In
+            </Form.Submit>
+          </Form.Base>
+
+          <Form.Text>
+            New to Netflix? <Form.Link to="/signup">Sign up now.</Form.Link>
+          </Form.Text>
+          <Form.TextSmall>
+            This page is protected by Google reCAPTCHA to ensure you're not a bot. Learn more.
+          </Form.TextSmall>
+        </Form>
+      </HeaderContainer>
+      <FooterContainer />
+    </>
+  );
+}
