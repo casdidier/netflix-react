1bcacaeb45005cb727dbdbf5309e7de48dc70199
casdidier <cheung.didier@gmail.com>
2020-11-04 11:53:12 +0100

    firebase connection + seed database

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import App from './app';
 import 'normalize.css';
 import { GlobalStyles } from './global-styles'; // styled component Global style
+import { firebase } from './lib/firebase.prod';
 
 ReactDOM.render(
   <>
--- src/lib/firebase.prod.js
+++ src/lib/firebase.prod.js
@@ -5,17 +5,17 @@ import 'firebase/auth';
 // 1) when seeding the database you'll have to uncomment this!
 // import { seedDatabase } from '../seed';
 
-const config = {
-  apiKey: '',
-  authDomain: '',
-  databaseURL: '',
-  projectId: '',
-  storageBucket: '',
-  messagingSenderId: '',
-  appId: '',
+var firebaseConfig = {
+  apiKey: "AIzaSyCt8EKCpIdKjHXxFF6nlsQBFU0R9mVYvTo",
+  authDomain: "netflix-c1e95.firebaseapp.com",
+  databaseURL: "https://netflix-c1e95.firebaseio.com",
+  projectId: "netflix-c1e95",
+  storageBucket: "netflix-c1e95.appspot.com",
+  messagingSenderId: "28287991243",
+  appId: "1:28287991243:web:e5a7de3306cdb5923c0a4e"
 };
 
-const firebase = Firebase.initializeApp(config);
+const firebase = Firebase.initializeApp(firebaseConfig);
 // 2) when seeding the database you'll have to uncomment this!
 // seedDatabase(firebase);
 // 3) once you have populated the database (only run once!), re-comment this so you don't get duplicate data
