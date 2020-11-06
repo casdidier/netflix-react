92b0b59b8905205ab478d1491027350bef1d2d1f
casdidier <cheung.didier@gmail.com>
2020-11-06 13:52:23 +0100

    protected routes

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import { JumbotronContainer } from './containers/jumbotron';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import { Browse, Signin, Signup, Home } from './pages';
 import * as ROUTES from './constants/routes';
-import { IsUserRedirect } from './helpers/routes';
+import { IsUserRedirect, ProtectedRoute } from './helpers/routes';
 
 export default function App() {
 
@@ -15,30 +15,18 @@ export default function App() {
 
   return (
     <Router>
-      <Route exact path={ROUTES.HOME}>
-        <JumbotronContainer />
-        <FaqsContainer />
-        <FooterContainer />
-      </Route>
-      <Route exact path={ROUTES.SIGN_IN}>
-        <IsUserRedirect
-         user={user}
-         loggedInPath={ROUTES.BROWSE}
-         path={ROUTES.SIGN_IN}
-        >
+      <IsUserRedirect user={user} loggedInPath={ROUTES.BROWSE} path={ROUTES.SIGN_IN}>
           <Signin />
         </IsUserRedirect>
-      </Route>
-      <Route exact path={ROUTES.BROWSE}>
-      <IsUserRedirect
-         user={user}
-         loggedInPath={ROUTES.BROWSE}
-         path={ROUTES.SIGN_UP}
-        >
+        <IsUserRedirect user={user} loggedInPath={ROUTES.BROWSE} path={ROUTES.SIGN_UP}>
           <Signup />
         </IsUserRedirect>
-      </Route>
-      <Route exact path={ROUTES.BROWSE}>
-        <Browse />
-      </Route>
-    </}
+        <ProtectedRoute user={user} path={ROUTES.BROWSE}>
+          <Browse />
+        </ProtectedRoute>
+        <IsUserRedirect user={user} loggedInPath={ROUTES.BROWSE} path={ROUTES.HOME}>
+          <Home />
+        </IsUserRedirect>
+    </Router>
+  );
+}
