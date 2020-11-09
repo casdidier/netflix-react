b0b09fce81e579d145244ea33a459028f47cd9a6
casdidier <cheung.didier@gmail.com>
2020-11-09 11:03:43 +0100

    custom hook useContent for browse

--- src/pages/browse.js
+++ src/pages/browse.js
@@ -1,5 +1,12 @@
 import React from 'react';
+import {useContent} from '../hooks';
 
 export default function Browse(params) {
+
+  const {series} = useContent('series');
+  const {films} = useContent('films');
+
+  console.log(series);
+
   return (<p>Hello Browse</p>)
 };
